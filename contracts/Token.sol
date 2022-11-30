6c199ee196fd580cb275706d18f632efe6c3332c
omkarg01 <omkargujja01@gmail.com>
2022-11-30 11:38:03 +0530

    Token SC

---
+++ contracts/Token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.5.0 <0.9.0;
+
+contract Token {
+    // declare name, symbol, totalSupply, owner, balances (shows balances to corresponding addressess)
+
+    // define a constructor which sets the owner and that owner will have the inital totalSupply balances
+
+    // define a function (transfer), it sends eth to specify address and amount to sent
+    // {
+        // check the sender has enough balance tokens
+
+        // deduct the amount from that address
+
+        // increase the amount in that address
+    // }
+
+    // define a func balanceOf, which returns the balance of specify address 
+
+}
